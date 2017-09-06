eb95c90a64b4a89746574d20a626e857e340cdbb
Dstack <dstack@163.com>
2017-09-06 17:54:03 +0800

    aaa

--- work/WorkDemo2.java
+++ work/WorkDemo2.java
@@ -1,11 +1,10 @@
 class WorkDemo2{
 	public static void main(String[] args){
-		Time t = new Time();
 		Car c1 = new Car("c1");
 		Car c2 = new Car("c2");
-		ThreedCar t1 = new ThreedCar("t1",t);
-		CowCar cow1 = new CowCar("cow1",t);
-		CowCar cow2 = new CowCar("cow2",t);
+		ThreedCar t1 = new ThreedCar("t1");
+		CowCar cow1 = new CowCar("cow1");
+		CowCar cow2 = new CowCar("cow2");
 		
 		c1.start();
 		c2.start();
@@ -17,55 +16,64 @@ class WorkDemo2{
 
 //2
 class Car extends Thread{
+	private int time = 0;
 	private String name;
-	private Time t;
-	public Car(String name,Time t){
+	
+	public Car(String name){
 		this.name = name;
-		this.t = t;
 	}
 	public synchronized void run(){
 		System.out.println(name +" is start...");
-		t.running();
+		while(time < 3){
+			time = time + 1;
+		}
+		try{
+			Thread.sleep(3000);
+			System.out.println(name + " is throngth: " + time);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 }
 
 //1
 class ThreedCar extends Thread{
-	private Time t;
+	private int time = 0;
 	private String name;
-	public ThreedCar(String name,Time t){
+	public ThreedCar(String name){
 		this.name = name;
-		this.t = t;
 	}
 	public synchronized void run(){
 		System.out.println(name +" is start...");
-		t.running();
+		while(time < 5){
+			time = time + 1;
+		}
+		try{
+			Thread.sleep(5000);
+			System.out.println(name + " is throngth: " + time);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 }
 
 //2
 class CowCar extends Thread{
 	private String name;
-	private Time t;
-	public CowCar(String name,Time t){
+	private int time = 0;
+	public CowCar(String name){
 		this.name = name;
-		this.t = t;
 	}
 	public synchronized void run(){
 		System.out.println(name +" is start...");
-		t.running();
-	}
-}
-
-class Time{
-	private int time = 0;
-	public synchronized void running(int time){
-		while(this.time < time){
-			this.time = this.time + 1;
+		while(time < 10){
+			time = time + 1;
 		}
 		try{
-			Thread.sleep(time*1000);
-			System.out.println(" is throngth: " + time);
+			Thread.sleep(10000);
+			System.out.println(name + " is throngth: " + time);
 		}
 		catch(Exception e){
 			e.printStackTrace();
